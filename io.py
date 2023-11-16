5143056e8000a6477e6fedd1e9f43a4221ed7fed
AndikaBN <bintangnursalih275@gmail.com>
2023-11-16 21:31:23 +0800

    first commit

---
+++ io.py
@@ -0,0 +1,11 @@
+angka1 = int(input("angka 1:"))
+angka2 = int(input("angka 2:"))
+print( angka1 + angka2 )
+
+# ini adalah inline comment
+
+'''
+ini adalah
+multiline
+commnet
+'''
---
+++ variabel.py
@@ -0,0 +1,9 @@
+name = "andika";
+umur = 19;
+salary = 5000.00,
+listNamaDev = ['andikadev','rikodev']
+print(name)
+print(f'{umur} tahun')  
+print(type(salary));
+print(type(umur));
+print(listNamaDev[0]);
