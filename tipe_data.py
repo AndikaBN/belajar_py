5ea02e0c80c441efb7493c5f6f478e8e9228b350
AndikaBN <bintangnursalih275@gmail.com>
2023-11-16 21:49:12 +0800

    tipe data

---
+++ tipe_data.py
@@ -0,0 +1,15 @@
+p = 30;
+l = 23
+t = 43
+
+volumePersegiPanjang = p * l * t
+
+print('volume balok adalah = ' f'{volumePersegiPanjang}'' km3');
+
+#=============================
+
+def f(x):
+    return x**2
+
+result = f(3)
+print(result)
--- variabel.py
+++ variabel.py
@@ -1,9 +1,10 @@
 name = "andika";
 umur = 19;
 salary = 5000.00,
-listNamaDev = ['andikadev','rikodev']
+listNamaDev = ['andikadev','rikodev','yosidev']
 print(name)
 print(f'{umur} tahun')  
+print('hallo nama saya adalah'f' {name}')  
 print(type(salary));
 print(type(umur));
 print(listNamaDev[0]);
